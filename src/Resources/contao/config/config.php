5998d58f7bb6b062fd6957cdf8fab45a83cfbe6b
Holger Neuner <neuner@47gradnord.de>
2018-12-28 11:05:35 +0100

    try to update some things

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Themepack - to produce Websites using Theme, 47GradNord - Agentur für Internetlösungen  ThemepackBundle
+ *
+ * @copyright  Copyright (c) 2008-2018, 47GradNord - Agentur für Internetlösungen
+ * @author     47GradNord - Agentur für Internetlösungen <info@47gradnord.de>
+ */
---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 47GN THEMEPACK for Contao Open Source CMS
+ *
+ * Copyright (C) 2018 47GradNord - Agentur für Internetlösungen
+ *
+ * @license    commercial
+ * @author     Holger Neuner
+ */
+
+$GLOBALS['TL_LANG']['CTE']['tp_elements'] = 'Themepack - Elemente';
+$GLOBALS['TL_LANG']['CTE']['tp_bundles'] = 'Themepack - Bundles';
