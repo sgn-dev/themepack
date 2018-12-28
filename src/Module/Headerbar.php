31ae9dfc192ec4414c3edaa8fc4290f4636eb5b3
Holger Neuner <neuner@47gradnord.de>
2018-12-28 11:26:22 +0100

    dev

---
+++ src/Module/Headerbar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 47GN THEMEPACK for Contao Open Source CMS
+ *
+ * Copyright (C) 2018 47GradNord - Agentur für Internetlösungen
+ *
+ * @license    commercial
+ * @author     Holger Neuner
+ */
+
+
+namespace Sgn47gradnord\Themepack\Module;
+
+
+class Headerbar extends AbstractModule
+{
+
+}
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,3 +8,15 @@ declare(strict_types=1);
  * @copyright  Copyright (c) 2008-2018, 47GradNord - Agentur für Internetlösungen
  * @author     47GradNord - Agentur für Internetlösungen <info@47gradnord.de>
  */
+
+/**
+ * Content Elements
+ */
+$GLOBALS['TL_CTE']['themepack-elements']['test'] = 'ThemepackBundle\Frontend\Element\TextIntroduction';
+
+/**
+ * Frontend Modules
+ */
+$GLOBALS['FE_MOD']['themepack'] = [
+    'tp_headerbar'         => 'ThemepackBundle\Module\Headerbar'
+];
